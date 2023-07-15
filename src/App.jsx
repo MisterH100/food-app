6e245ce027cd636a8e743bc837537bd3d48ad6d0
misterh100 <hansomenyathi1@gmail.com>
2023-07-16 00:42:09 +0200

    innitial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./components/navbar"
+import Banner from "./components/banner"
+import Food from "./components/food"
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Banner/>
+      <Food/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+function NavBar() {
+    return (
+        <nav className="w-full">
+            <h1 className="w-[200px] h-full text-center text-[30px] m-auto">Sizzle</h1>
+            <p className="w-[200px] text-center text-[16px] m-auto">a BBQ place</p>
+        </nav>
+    )
+}
+
+export default NavBar;
